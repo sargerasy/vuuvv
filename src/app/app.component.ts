5a6cd7d9ffaa65378944c0ed7c76ac0dceee2ec5
ZengYing <vuuvv@qq.com>
2016-06-12 14:58:09 +0800

    make project works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import {Component} from '@angular/core';
 import '../styles.scss';
 
 @Component({
-  selector: 'my-app',
+  selector: 'vu-app',
   template: require('./app.component.html'),
   styles: [require('./app.component.scss')]
 })
 
 export class AppComponent {
- 
+  defaultMeaning: number = 42;
+
+  meaningOfLife(meaning: number) {
+    return `The meaning of life is ${meaning || this.defaultMeaning}`
+  }
 }
