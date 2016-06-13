5a0150b90d8dd131432ffdd23892190ff2d32233
ZengYing <vuuvv@qq.com>
2016-06-13 17:39:18 +0800

    add first real unit test

--- src/components/button/button.spec.ts
+++ src/components/button/button.spec.ts
@@ -10,25 +10,40 @@ import {
   beforeEach
 } from '@angular/core/testing';
 import { TestComponentBuilder } from '@angular/compiler/testing';
+// import { AsyncTestFn } from '@angular/core/testing';
 import { provide, Component } from '@angular/core';
+import { DebugElement } from '@angular/core';
 
 import { MdButton } from './button';
+
 
 describe('MdButton', () => {
   let builder: TestComponentBuilder;
 
   beforeEach(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => { builder = tcb; }));
 
-  it('should ...', () => {
-    return builder.createAsync(<any>MdButton).then((fixture) => {
-      fixture.detectChanges();
+  describe('button[md-button]', () => {
+    it('should handle a click on the button', (done: () => void) => {
+      return builder.createAsync(TestApp).then((fixture) => {
+        let testComponent = fixture.debugElement.componentInstance;
+        let buttonDebugElement = getChildDebugElement(fixture.debugElement, 'button');
+
+        buttonDebugElement.nativeElement.click();
+        expect(testComponent.clickCount).toBe(1);
+        done();
+      });
     });
   });
 });
 
+/** Gets a child DebugElement by tag name. */
+function getChildDebugElement(parent: DebugElement, tagName: string): DebugElement {
+  return parent.query(debugEl => debugEl.nativeElement.tagName.toLowerCase() === tagName);
+}
+
 @Component({
   selector: 'test-app',
-  template: `<button md-button type="button" (click)="increment()"`,
+  template: `<button md-button type="button" (click)="increment()" [disabled]="isDisabled">Go</button>`,
   directives: [MdButton],
 })
 class TestApp {
