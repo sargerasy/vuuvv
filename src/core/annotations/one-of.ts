6d60cc492f18a957686ad0e61b16a4b819d03b4e
ZengYing <vuuvv@qq.com>
2016-06-15 16:42:43 +0800

    ui(): add dir directive, add backgroun palette

---
+++ src/core/annotations/one-of.ts
@@ -0,0 +1,31 @@
+import { isPresent } from '@angular/common/src/facade/lang';
+
+declare var Symbol: any;
+
+/**
+ * Annotation Factory that only allows a set of values to be set for aproperty.
+ * @param values The list of allowed values. The first value listed will be used as a default if
+ *    the set value isn't part of the list of accepted values.
+ */
+ function OneOfFactory(values: any[]) {
+   return function OneOfMetadata(target: any, key: string): void {
+     const defaultValue = values[0];
+
+     // Use a fallback if Symbol isn't available.
+     const localKey = isPresent(Symbol) ? Symbol(key) : `@@$${key}_`;
+     target[localKey] = defaultValue;
+
+     Object.defineProperty(target, key, {
+       get() { return this[localKey]; },
+       set(v) {
+         if (values.indexOf(v) === -1) {
+           this[localKey] = defaultValue;
+         } else {
+           this[localKey] = v;
+         }
+       }
+     });
+   };
+ }
+
+ export let OneOf = OneOfFactory;
